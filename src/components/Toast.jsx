2f73d83fd4250ab0c6e8f10de9dadf9a66731441
unknown <ama991969@gmail.com>
2025-02-03 12:19:19 +0200

    Updata Protfolio

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 export default function Toast() {
-    const {t} = useTranslation()
+  const { t } = useTranslation();
   return (
-   <>
-   <div className='p-3 shadow-md rounded-md fixed  z-[9999]  top-16 left-1/2 -translate-x-1/2 bg-white'>
-   <h2 className='text-main-color text-center'> ✔ {t("toastmsg")}</h2>
-   </div>
-   </>
-  )
+    <>
+      <div className="p-3 shadow-md rounded-md fixed  z-[9999]  top-16 left-1/2 -translate-x-1/2 bg-white">
+        <h2 className="text-main-color text-center"> ✔ {t("toastmsg")}</h2>
+      </div>
+    </>
+  );
 }
