9742224023ffc0396a697072fc604b20f2c0f034
unknown <ama991969@gmail.com>
2025-02-04 09:30:39 +0200

    Updata Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Suspense, useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Header from "./layouts/Header/Header";
 import LoadingSpinner from "@/utils/LoadingSpinner";
 import ScrollTopBtn from "@/utils/ScrollTopBtn";
+import Header from "./layouts/header/Header";
 const UiUxService = React.lazy(() => import("@/pages/UiUxService"));
 const WebService = React.lazy(() => import("@/pages/WebService"));
 const MobileService = React.lazy(() => import("@/pages/MobileService"));
