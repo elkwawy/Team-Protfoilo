1489a431a9cda6938d1f40b9258bdfcef9dc6aab
unknown <ama991969@gmail.com>
2025-02-07 18:18:57 +0200

    fixed problem mobile menu in header and edit data ourServices

--- src/data/ourServices.js
+++ src/data/ourServices.js
@@ -1,14 +1,14 @@
 export const ourServices = [
   {
     title: "servTitle1",
-    link: "/services/uiux_services",
+    link: "uiux_services",
   },
   {
     title: "servTitle2",
-    link: "/services/web_services",
+    link: "web_services",
   },
   // {
   //   title: "servTitle3",
-  //   link: "/services/mobile_app_services",
+  //   link: "mobile_app_services",
   // },
 ];
--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Img } from "react-image";
-import LoadingSpinner from "@/utils/LoadingSpinner";
 import LanguageSelector from "./components/LanguageSelector";
 import BtnContactUs from "./components/BtnContactUs";
 import NavMenu from "./components/NavMenu";
@@ -15,7 +13,7 @@ const Header = () => {
   const [isServices, setIsServices] = useState(false);
 
   return (
-    <header className="py-2 bg-white shadow">
+    <header className="py-2 bg-white shadow relative">
       <div className="containerD flex flex-wrap justify-between items-center">
         {/* Logo */}
         <div className="flex items-center  ">
@@ -24,7 +22,9 @@ const Header = () => {
               src="/assets/.codeLogo/logo.webp"
               alt="DotCode"
               parent={`w-14 h-14`}
-              className={`w-full h-full ${selectedLanguage === "ar" ? "ml-6" : ""}`}
+              className={`w-full h-full ${
+                selectedLanguage === "ar" ? "ml-6" : ""
+              }`}
               // loader={<LoadingSpinner />}
             />
           </Link>
@@ -41,11 +41,7 @@ const Header = () => {
         />
 
         {/* Buttons Selection */}
-        <div
-          className={`flex items-center gap-2 ${
-            selectedLanguage === "ar" && "max-[900px]:-mr-6"
-          } `}
-        >
+        <div className={`flex items-center gap-2`}>
           <LanguageSelector
             selectedLanguage={selectedLanguage}
             setSelectedLanguage={setSelectedLanguage}
@@ -65,24 +61,24 @@ const Header = () => {
         />
       </div>
       <div
-        className={`transition-all duration-500 ease-in-out transform ${
+        className={`transition-all duration-500 my-2 ease-in-out transform ${
           isMenuOpen
             ? "max-h-96 opacity-100"
             : "max-h-0 opacity-0 overflow-hidden"
-        }`}
+        } absolute top-[57px] w-full bg-white z-50`}
       >
-        <div className="flex flex-col max-[900px]:mt-2 max-[900px]:pt-2 max-[900px]:border-t gap-2 items-center justify-center">
+        <div className="flex flex-col max-[900px]:mt-2 max-[900px]:pt-3 max-[900px]:border-y shadow gap-2 items-center justify-center">
           <NavMenu
             selectedLanguage={selectedLanguage}
             setIsMenuOpen={setIsMenuOpen}
             setIsServices={setIsServices}
             isServices={isServices}
-            className={`min-[900px]:hidden flex-col gap-4 z-40`}
+            className={`min-[900px]:hidden flex-col min-[388px]:mb-3 gap-4 z-40`}
           />
           <BtnContactUs
             selectedLanguage={selectedLanguage}
             setIsMenuOpen={setIsMenuOpen}
-            className="min-[388px]:hidden"
+            className="min-[388px]:hidden mb-3"
           />
         </div>
       </div>
